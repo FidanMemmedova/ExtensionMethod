111aa52d0d3caf95d10576c8fe5c4e7dfdc8546c
Fidan Mammadova <100687487+FidanMemmedova@users.noreply.github.com>
2022-04-05 11:52:44 +0400

    Add files via upload

---
+++ FidanCyhn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FidanCyhn
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string word = "Salam";
+            word.CustomContains('A');
+            int num = 44;
+            num.IsPrime();
+        }
+    }
+}
+    
